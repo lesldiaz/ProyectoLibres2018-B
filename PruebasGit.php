b48b3b46c31a245acbd07d6debf0c18c670b12f2
Leonix23 <leo_andres1995@hotmail.es>
2018-12-18 19:44:18 -0500

    Create PruebasGit.php

---
+++ PruebasGit.php
@@ -0,0 +1,29 @@
+<?php
+    require_once "pdo.php";
+    require_once "delete_files.php";
+
+	//eliminar objetoaprendizaje de aprendizaje
+	
+    function deleteOA($filename, $idOA) {
+        $name = basename($filename,".zip");
+        $target = 'oa/' . $name . '/';
+        delete_files($target);
+    
+        $dirZip = 'zip/' . $filename;
+        unlink($dirZip);
+		
+		$pdo = new PDO('mysql:host=localhost;dbname=sistemaoa;charset=utf8', 'root','');
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		$sql = "DELETE FROM puntuacion WHERE idObjetosAprendizaje = :idOA";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(array(':idOA' => $idOA));
+		
+		$sql = "DELETE FROM objetoaprendizaje WHERE idOA = :idOA";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(array(':idOA' => $idOA));
+
+        $sql = "DELETE FROM comentario WHERE idOA = :idOA";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(array(':idOA' => $idOA));
+    }
