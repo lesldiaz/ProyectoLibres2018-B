7e58cbb5f207980e831166fbabe65800befdf573
Leonix23 <leo_andres1995@hotmail.es>
2019-01-02 23:53:20 -0500

    Update PruebasGit.php

--- PruebasGit.php
+++ PruebasGit.php
@@ -14,7 +14,8 @@
 		
 		$pdo = new PDO('mysql:host=localhost;dbname=sistemaoa;charset=utf8', 'root','');
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		
+
+			
 		$sql = "DELETE FROM puntuacion WHERE idObjetosAprendizaje = :idOA";
         $stmt = $pdo->prepare($sql);
         $stmt->execute(array(':idOA' => $idOA));
@@ -22,7 +23,8 @@
 		$sql = "DELETE FROM objetoaprendizaje WHERE idOA = :idOA";
         $stmt = $pdo->prepare($sql);
         $stmt->execute(array(':idOA' => $idOA));
-
+		
+		//eliminar comentario
         $sql = "DELETE FROM comentario WHERE idOA = :idOA";
         $stmt = $pdo->prepare($sql);
         $stmt->execute(array(':idOA' => $idOA));
