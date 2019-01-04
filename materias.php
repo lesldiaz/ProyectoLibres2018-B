66ea9e8845782026bd6b38ec73aefba004d8864c
Mishu95 <tefi_0095@outlook.com>
2019-01-03 23:12:24 -0500

    Update materias.php

--- materias.php
+++ materias.php
@@ -1,13 +1,11 @@
 <?php
-
-
     require_once "pdo.php";
     $nombreCarrera=$_POST['nombreCarrera'];
     $lstMaterias= array();               // <array para la lista de materias-->
     $sql="CALL consultarMaterias(:nameCarrera)";
     $stmt = $pdo->prepare($sql);
     $stmt->execute(array(':nameCarrera' => $nombreCarrera)); 
-    while ($row = $stmt->fetch()){    //llenar aray de lista de maerias
+    while ($row = $stmt->fetch()){    
         array_push($lstMaterias, $row);
     }
     echo json_encode($lstMaterias);
