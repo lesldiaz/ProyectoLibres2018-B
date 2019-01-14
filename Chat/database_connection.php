4f49b402c273129a3f6b70dfea7fa2f3ec419a5f
Leonix23 <leo_andres1995@hotmail.es>
2019-01-14 15:21:25 -0500

    Update database_connection.php

--- Chat/database_connection.php
+++ Chat/database_connection.php
@@ -127,7 +127,7 @@ function fetch_is_type_status($user_id, $connect)
 }
 
 
-public function modUsuario() {
+//public function modUsuario() {
         $conexion = sistemaoa::connectDB();
         $modificacion = "UPDATE user SET nombre=\"$this->nombre\", apellido1=\"$this->apellido1\", "
                 . "apellido2=\"$this->apellido2\""
